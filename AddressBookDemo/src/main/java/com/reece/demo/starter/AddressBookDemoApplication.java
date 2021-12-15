ce5277f03c5894b309f6db0ff84a780e1878d7c8
Bharathy Poovalingam <bharathy.poovalingam@gmail.com>
2021-12-15 11:40:54 +1100

    Initial commit to bootstrap the project

---
+++ AddressBookDemo/src/main/java/com/reece/demo/starter/AddressBookDemoApplication.java
@@ -0,0 +1,13 @@
+package com.reece.demo.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookDemoApplication.class, args);
+	}
+
+}
---
+++ AddressBookDemo/src/test/java/com/reece/demo/starter/AddressBookDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reece.demo.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
