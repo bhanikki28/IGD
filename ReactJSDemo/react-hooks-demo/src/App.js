c49dedefb34d161c1efd3a0a29e7e9d38f764dc1
Bharathy Poovalingam <bharathy.poovalingam@gmail.com>
2022-01-22 09:38:49 +1100

    ReactHooks : useState hook explained

---
+++ ReactJSDemo/react-hooks-demo/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import StateHookDemo from './component/StateHookDemo';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div>
+          <StateHookDemo />
+      </div>
+    </div>
+  );
+}
+
+export default App;
