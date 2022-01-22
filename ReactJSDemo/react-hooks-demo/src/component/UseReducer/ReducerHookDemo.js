e59c792e457fb92ef2fa824dc9270ef0aa80436a
Bharathy Poovalingam <bharathy.poovalingam@gmail.com>
2022-01-22 11:26:11 +1100

    ReducerHook Demo

---
+++ ReactJSDemo/react-hooks-demo/src/component/UseReducer/ReducerHookDemo.js
@@ -0,0 +1,64 @@
+import React, {useState,useReducer} from "react";
+
+const ReducerHookDemo = () => {
+    /*
+    // without using Reducer
+    const[count,setCount] = useState(0);
+    const[showCounter,setShowCounter] = useState(true);
+    return (
+        <div>
+            <h1> {count} </h1>
+            <button 
+            onClick = {() => {
+                setCount(count + 1);
+                setShowCounter(!showCounter)
+            }}
+            >
+                Click Here
+            </button>
+
+            { showCounter && <p> Count Value is : {count}</p> }
+        </div>
+
+
+    ); */
+
+    const reducer = (state,action) => {
+
+        switch(action.type){
+            case "Increment" : 
+                return {count : state.count+1, showCounter : state.showCounter}
+            case "toggleShowCount" :
+                return {count : state.count, showCounter : !state.showCounter}
+            default:
+                return state
+        }
+
+    };
+    const [state,dispatch] = useReducer(reducer, 
+        {count: 0,showCounter:true}
+    
+        )
+   
+    return (
+        <div>
+            <h1> {state.count} </h1>
+            <button 
+            onClick = {() => {
+                dispatch({type :"Increment"});
+                dispatch({type :"toggleShowCount"});
+
+            }}
+            >
+                Count Incrementer
+            </button>
+
+            { state.showCounter && <p> Count Value is : {state.count}</p> }
+        </div>
+
+
+    ); 
+
+};
+
+export default ReducerHookDemo;
